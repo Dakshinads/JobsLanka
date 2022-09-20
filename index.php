6850e9e2977fcb965c34aba78f87cf5b5448be61
DAKSHINA-LAPTOP\DAKSHINA-LAPTOP <dakshinashashimal.ds@gmail.com>
2022-09-21 00:03:10 +0530

    Footer initialized

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jobs Lanka Home Page</title>
+    <link href="assets\css\bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .mybgColor{
+        background-color:#80b3ff;
+      }
+    </style>
+</head>
+<body>
+    
+
+
+<main class="container">
+
+</main>
+
+<div class="b-example-divider"></div>
+<div class="">
+<div class="container ">
+  <footer class="py-5 border-top">
+    <div class="row">
+      <div class="col-6 col-md-6 mb-3">
+        <a href="#" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+            <img src="Images/logo.jpg"  width="150" height="150"/>
+          </a>
+      </div>
+
+      <div class="col-3 col-md-3 mb-3">
+        <ul class="nav flex-column">
+        <h5>About</h5>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About us</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contact us</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Inquiry</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQ</a></li>
+        </ul>
+      </div>
+
+      <div class="col-3 col-md-3 mb-3">
+        <ul class="nav flex-column">
+            <h5>For Job seekers and Employers</h5>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Login</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Post a Job</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Give a Feedback</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="   py-4 border-top">
+      <p class="text-center">System Breakers &copy; 2022.</p>
+    </div>
+  </footer>
+</div>
+</div>
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
